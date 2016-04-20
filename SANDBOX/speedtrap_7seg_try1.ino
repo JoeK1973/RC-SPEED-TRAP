18f816ec77ede8e0eb3fe90957f0e1a814ea395c
Joseph Keaveney <joe.keaveney@googlemail.com>
2016-04-20 09:52:25 +0100

    Update and rename speedtrap_7seg.ino to speedtrap_7seg_try1.ino

--- SANDBOX/speedtrap_7seg_try1.ino
+++ SANDBOX/speedtrap_7seg_try1.ino
@@ -2,6 +2,8 @@ unsigned long count_start;
 unsigned long count_stop;
 unsigned long count_time;
 
+int count_started = 0; // 0 for count not started, 1 for count started
+
 int rcvr_one = 2;
 int rcvr_two = 3;
 int direction;
@@ -13,9 +15,12 @@ void setup(){
 }
 
 void loop(){
-  if (digitalRead(rcvr_one) == HIGH && digitalRead(rcvr_two) == LOW){
+  if (count_started == 0){
+    if (digitalRead(rcvr_one) == LOW && digitalRead(rcvr_two) == HIGH){
+    count_started == 1;
     direction = 1;
-  } else if (digitalRead(rcvr_one) == LOW && digitalRead(rcvr_two) == HIGH){
+  } else if (digitalRead(rcvr_one) == HIGH && digitalRead(rcvr_two) == LOW){
+    count_started == 1;
     direction = 2;
   } else {
     direction = 0;
@@ -25,7 +30,11 @@ void loop(){
     timing();
   }
 }
+}
 
 void timing(){
-  
+  count_start = micros();
+  if (direction == 1){
+    
+  }
 }
